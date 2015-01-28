ca5b94b541e2f31600f3cb0d9d207c9370280d39
kimheeeun <khe930718@gmail.com>
2015-01-28 21:54:25 +0900

    webapp : 회원가입 기능 구현 수정(폰 번호 공개여부 추가)

--- sns/forms.py
+++ sns/forms.py
@@ -4,21 +4,19 @@ from sns.models import *
 
 
 class JoinForm(UserCreationForm):
-    phone = forms.CharField(max_length=20)
-    area = forms.ModelChoiceField(queryset=Area.objects)
-    image = forms.ImageField(required=False)
+    phone = forms.CharField(max_length=20, label='폰 번호')
+    is_public = forms.BooleanField(required=False, label='공개여부')
+    area = forms.ModelChoiceField(queryset=Area.objects, label='거주지역')
 
     class Meta:
         model = User
-        fields = ("last_name", "first_name", "username", "password1", "password2", "phone", "email", "area", "image")
+        fields = ("last_name", "first_name", "username", "password1", "password2", "phone", "is_public", "email", "area")
 
-    def save(self, commit=True):
+    def save(self, commit=False):
         user = super(JoinForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
         user.save()
-        student = Student(user=user, contact=self.cleaned_data['phone'], area=self.cleaned_data['area'], image=self.cleaned_data['image'])
+        student = Student(user=user, phone=self.cleaned_data['phone'], is_public=self.cleaned_data['is_public'], area=self.cleaned_data['area'])
         student.save()
         return user
-
-
 
--- sns/models.py
+++ sns/models.py
@@ -13,21 +13,24 @@ class Area(models.Model):
 
 class Student(models.Model):
     user = models.OneToOneField(User)
-    contact = models.CharField(max_length=20, blank=True, null=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    is_public = models.BooleanField(default=False)
     area = models.ForeignKey(Area)
-    image = models.ImageField(upload_to="/profile", default="default/avatar.png")
+    image = models.ImageField(upload_to="/profile", null=True, blank=True)
 
     def __str__(self):
-        return self.get_name() + "(" + self.get_username() + ")"
+        return self.get_name() + "(" + self.user.get_username() + ")"
 
     def get_name(self):
         return self.user.last_name + self.user.first_name
 
-    def get_username(self):
-        return self.user.username
+    def get_phone(self):
+        phone = self.phone if self.is_public else "010-xxxx-xxxx"
+        return phone
 
-    def get_area(self):
-        return self.area.name
+    def get_image(self):
+        image = self.image if self.image else "default/avatar.png"
+        return image
 
 
 class Category(models.Model):
