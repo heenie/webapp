4972fd68d23acfd0e2fea45eeb5e8c849658481c
LeeYoungIn <pimery@nate.com>
2015-02-06 20:06:00 +0900

    base- add footer

--- sns/views.py
+++ sns/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response, render
 from django.http import Http404, HttpResponseRedirect
 from django.core.urlresolvers import  reverse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import CreateView, View, DetailView, ListView
+from django.views.generic import CreateView, View, DetailView
 from sns.forms import JoinForm, WriteForm, ArticleForm
 from sns.models import Article
 from sns.forms import JoinForm, SearchForm
@@ -20,15 +20,15 @@ def index(request):
 #     return render_to_response('newsfeed.html', None)
 
 
-class Newsfeed(ListView):
+class Newsfeed(CreateView):
     template_name = "newsfeed.html"
     model = Article
-    # form_class = SearchForm
+    form_class = SearchForm
 
-    def get_context_data(self, **kwargs):
-        context = super(Newsfeed, self).get_context_data(**kwargs)
-        context.update({"filter_form": SearchForm(self.request.GET)})
-        return context
+
+    # def get(self, request):
+    #     form = self.form_class()
+    #     return render(request, self.template_name, {'form': form})
 
 
 class JoinView(CreateView):
@@ -46,6 +46,7 @@ def search(request):
 #     model = Article
 #     form_class = SearchForm
 #     success_url = "/"
+
 
 def ArticleView(request):
     # template_name = "article.html"
@@ -84,9 +85,11 @@ def login_view(request):
         else: return render_to_response('login.html', None)
     else: return  render_to_response('join.html', None)
 
+
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
+
 
 def loginuser(request):
     username=None
